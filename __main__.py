52aea300d335c930df2ee5126170f9224df312e6
Hundera Awoke <hunderaweke@gmail.com>
2024-06-23 15:38:31 +0300

    finished: searching and other functionalities

--- __main__.py
+++ __main__.py
@@ -1,19 +1,27 @@
+import os
+
+from pathlib import Path
 from preprocessors.tokenizer import Tokenizer
-from preprocessors.stemmer import MainStemmer
+from preprocessors.stemmer import MainStemmer, PorterStemmer
+from ranker.vector_space import TfIdfVectorizer
 from utils.index_file import IndexFileBuilder
 
-documents = [
-    "The quick brown fox jumps over the lazy dog.",
-    "The quick red fox jumps over the sleepy cat.",
-    "A fast blue fox leaps over the tired dog.",
-    "Cunning foxes are jumping over a lazy hound.",
-    "A group of lazy dogs were being jumped over by quick foxes.",
-    "The tired fox quickly leapt over the sleeping cat.",
-    "Several brown foxes have jumped over lazy dogs.",
-    "A quick fox will jump over the lazy dog.",
-    "The lazy dog was jumped over by a quick fox.",
-    "Quick brown foxes are leaping over lazy dogs.",
-    "The sleepy cat was being jumped over by the fast fox.",
-]
+index_to_name = {}
+CORPUS_PATH = Path.cwd() / "corpus"
+documents = []
+for i, file_name in enumerate(os.listdir(CORPUS_PATH)):
+    index_to_name[i] = file_name
+    with open(CORPUS_PATH / file_name, "r+") as file:
+        documents.append(file.readline().rstrip())
 
-print(IndexFileBuilder.build(documents))
+IndexFileBuilder.build(documents)
+IndexFileBuilder.create_index_file()
+IndexFileBuilder.create_posting_file()
+model = TfIdfVectorizer(len(documents))
+model.calculate_tf_idf()
+print("Welcome to Simple Information Retrieval System")
+query = input("Please Enter a query for searching in the documents: ")
+query = MainStemmer.stem(Tokenizer.tokenize(query))
+print("Here is the rank of the documents for the given query.")
+for i, indx in enumerate(model.rank_docs(query), start=1):
+    print(i, index_to_name[indx])
